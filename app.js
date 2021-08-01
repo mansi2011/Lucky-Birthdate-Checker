39d7426a5e57e1118e7d8ac085c943267a90ad1f
Mansi Srivastava <srivastavamansi91@gmail.com>
2021-08-01 20:40:00 +0530

    modified in logic

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 let crossBtn = document.querySelector('#close');
 let noticeDiv = document.querySelector('.notice')
-let dob = document.querySelector('#input-date');
-let number = document.querySelector('#input-number');
+let dobInput = document.querySelector('#input-date');
+let numberInput = document.querySelector('#input-number');
 let btnCheck = document.querySelector('#btn-check');
 let sadDiv = document.querySelector('.sad-div');
 let happyDiv = document.querySelector('.happy-div');
 
+var xyz = dobInput.value
+var abc = numberInput.value
+
+console.log(typeof(dobInput.value))
+console.log(xyz,abc)
 
 function checkLuckyDob(dob,num){
     let sum = 0 ;
     for (let i=0; i<dob.length; i++){
-        if (dob[i] == '/'){
+        if (dob[i] == '-'){
             continue
         }
         else{
             sum += dob[i]
         }
     }
-    if (dob % num == 0){
-        console.log("happy")
-        happyDiv.style.display = "block"
+    if (sum % num == 0){
+        console.log("happy");
+        happyDiv.style.display = "block";
+        sadDiv.style.display = "none";
     }
     else{
-        console.log("sad")
-        sadDiv.style.display = "block"
+        console.log("sad");
+        sadDiv.style.display = "block";
+        happyDiv.style.display = "none";
     }
 }
 
@@ -31,7 +38,7 @@ function checkLuckyDob(dob,num){
 
 
 btnCheck.addEventListener('click', function(){
-    checkLuckyDob(dob.value,number.value)
+    checkLuckyDob(dobInput.value,numberInput.value)
 })
 
 crossBtn.addEventListener('click', ()=>{
